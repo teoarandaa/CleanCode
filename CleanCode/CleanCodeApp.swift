368023614f253a25e0d8be8e34d40e49fb4e8c92
Teo Aranda Páez <145863288+teoarandaa@users.noreply.github.com>
2024-09-13 19:28:39 +0200

    Initial Commit

---
+++ CleanCode/CleanCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanCodeApp.swift
+//  CleanCode
+//
+//  Created by Teo Aranda Páez on 13/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CleanCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

adefadaa863896bcd7ce855ec891fbb4f12f8747
Teo Aranda Páez <145863288+teoarandaa@users.noreply.github.com>
2024-10-22 19:11:11 +0200

    mentions document created and onboarding screen base added

--- CleanCode/ContentView.swift
+++ CleanCode/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     let language = Bundle.main.decode([MenuSection].self, from: "menu.json")
     @State private var showingBottomSheet: Bool = false
     @AppStorage("isDarkMode") private var isDarkMode = false
+    @AppStorage("iswelcomeSheetShowing") var isWelcomeSheetShowing: Bool = true
     
     var body: some View {
         NavigationStack {
@@ -51,9 +52,76 @@ struct ContentView: View {
                 .presentationDetents(.init([.height(700)]))
                 .presentationDragIndicator(.visible)
         }
+        .sheet(isPresented: $isWelcomeSheetShowing) {
+            WelcomeView(isWelcomeSheetShowing: $isWelcomeSheetShowing)
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    WelcomeView(isWelcomeSheetShowing: .constant(false))
+}
+
+struct PageInfo: Identifiable {
+    let id = UUID()
+    let label: String
+    let text: String
+    let image: ImageResource
+}
+
+let pages = [
+    PageInfo(label: "welcome to cleancode", text: "discover the finest pastries", image: .imageOne),
+    PageInfo(label: "omg to cleancode", text: "hell yeah", image: .imageOne),
+    PageInfo(label: "last to cleancode", text: "bye bye", image: .imageOne)
+]
+
+struct WelcomeView: View {
+    @Binding var isWelcomeSheetShowing: Bool
+    @State private var currentPage = 0 // Para rastrear la página actual
+    
+    var body: some View {
+        VStack {
+            TabView(selection: $currentPage) {
+                ForEach(0..<pages.count, id: \.self) { index in
+                    VStack {
+                        Text(pages[index].label)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                        
+                        Text(pages[index].text)
+                            .fontWeight(.medium)
+                            .padding()
+                        
+                        Image(pages[index].image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding()
+                    }
+                    .tag(index) // Etiqueta para la selección de TabView
+                }
+            }
+            .tabViewStyle(.page)
+            
+            // Botón con animación de aparición/desaparición basado en la página actual
+            Button {
+                isWelcomeSheetShowing.toggle()
+            } label: {
+                Text("Get started")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 8)
+            }
+            .buttonStyle(.borderedProminent)
+            .padding()
+            .opacity(currentPage == pages.count - 1 ? 1 : 0) // El botón aparece solo en la última página
+            .animation(.easeInOut(duration: 0.5), value: currentPage) // Animación al cambiar de página
+        }
+        .interactiveDismissDisabled()
+        .onAppear {
+            UIPageControl.appearance().currentPageIndicatorTintColor = .label
+            UIPageControl.appearance().pageIndicatorTintColor = .systemGray
+        }
+    }
 }

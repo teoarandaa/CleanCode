5d7424678c4d2ecfde0b689dd0dd1488b70638f4
Teo Aranda Páez <145863288+teoarandaa@users.noreply.github.com>
2024-10-28 22:04:48 +0100

    Update CleanCodeApp.swift

--- CleanCode/CleanCodeApp.swift
+++ CleanCode/CleanCodeApp.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 @main
 struct CleanCodeApp: App {
+    // MARK: - Controls the mode (dark/light) on the app
     @AppStorage("isDarkMode") private var isDarkMode = false
     
     var body: some Scene {
         WindowGroup {
             ContentView()
+                // MARK: - Keeps the state of the variable isDarkMode when the color scheme is changed
                 .preferredColorScheme(isDarkMode ? .dark : .light)
         }
     }

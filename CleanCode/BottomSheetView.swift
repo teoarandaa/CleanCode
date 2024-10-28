08d3bf09b0cdc56f4df465f3310edcfca7298383
Teo Aranda Páez <145863288+teoarandaa@users.noreply.github.com>
2024-10-28 14:26:10 +0100

    updates bottomsheetview UI

--- CleanCode/BottomSheetView.swift
+++ CleanCode/BottomSheetView.swift
@@ -15,75 +15,60 @@ struct BottomSheetView: View {
         VStack(alignment: .leading) {
             
             // MARK: - Easy
-            ZStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundStyle(.regularMaterial)
-                    .frame(width: .infinity, height: 160)
-                HStack {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 100)
-                            .frame(width: 40, height: 40)
-                            .foregroundStyle(.green)
-                        Text("E")
-                            .font(.title.bold())
-                            .foregroundStyle(.white)
-                    }
-                    
-                    VStack(alignment: .leading) {
-                        Text("Easy")
-                            .font(.headline)
-                        Text("Accessible for beginners, with clear syntax and abundant support resources. Ideal for quickly learning the basics.")
-                    }
-                    .padding()
+            HStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 100)
+                        .frame(width: 40, height: 40)
+                        .foregroundStyle(.green)
+                    Text("E")
+                        .font(.title.bold())
+                        .foregroundStyle(.white)
                 }
+                
+                VStack(alignment: .leading) {
+                    Text("Easy")
+                        .font(.headline)
+                    Text("Accessible for beginners, with clear syntax and abundant support resources. Ideal for quickly learning the basics.")
+                }
+                .padding()
             }
             
             // MARK: - Intermediate
-            ZStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundStyle(.regularMaterial)
-                    .frame(width: .infinity, height: 180)
-                HStack {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 100)
-                            .frame(width: 40, height: 40)
-                            .foregroundStyle(.yellow)
-                        Text("I")
-                            .font(.title.bold())
-                            .foregroundStyle(.white)
-                    }
-                    
-                    VStack(alignment: .leading) {
-                        Text("Intermediate")
-                            .font(.headline)
-                        Text("Requires a deeper knowledge and understanding of concepts such as object-oriented programming and data structures. Presents significant challenges.")
-                    }
-                    .padding()
+            HStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 100)
+                        .frame(width: 40, height: 40)
+                        .foregroundStyle(.yellow)
+                    Text("I")
+                        .font(.title.bold())
+                        .foregroundStyle(.white)
                 }
+                
+                VStack(alignment: .leading) {
+                    Text("Intermediate")
+                        .font(.headline)
+                    Text("Requires a deeper knowledge and understanding of concepts such as object-oriented programming and data structures. Presents significant challenges.")
+                }
+                .padding()
             }
             
             // MARK: - Hard
-            ZStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundStyle(.regularMaterial)
-                    .frame(width: .infinity, height: 200)
-                HStack {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 100)
-                            .frame(width: 40, height: 40)
-                            .foregroundStyle(.red)
-                        Text("D")
-                            .font(.title.bold())
-                            .foregroundStyle(.white)
-                    }
-                    
-                    VStack(alignment: .leading) {
-                        Text("Hard")
-                            .font(.headline)
-                        Text("Complex languages that demand an advanced understanding. Involve memory management and performance optimization, being challenging even for experienced programmers.")
-                    }
-                    .padding()
+            HStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 100)
+                        .frame(width: 40, height: 40)
+                        .foregroundStyle(.red)
+                    Text("D")
+                        .font(.title.bold())
+                        .foregroundStyle(.white)
                 }
+                
+                VStack(alignment: .leading) {
+                    Text("Hard")
+                        .font(.headline)
+                    Text("Complex languages that demand an advanced understanding. Involve memory management and performance optimization, being challenging even for experienced programmers.")
+                }
+                .padding()
             }
         }
         .padding()
